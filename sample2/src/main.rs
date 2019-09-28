d93fa4a9d00f9a036d212d69410118d06dce2805
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-09-28 22:33:47 +0900

    update

--- sample2/src/main.rs
+++ sample2/src/main.rs
@@ -1,3 +1,6 @@
+use std::thread;
+use std::time::Duration;
+
 struct Person {
     name: String,
 }
@@ -11,16 +14,48 @@ impl Person {
 
     fn eat(&self) {
         println!("eat: {}", self.name);
+
+        thread::sleep(Duration::from_millis(1000));
+    }
+
+    fn hoge(&mut self) {
+        self.name = "hoge".to_string();
+        self.fuga();
+        self.name = "hoge".to_string();
+    }
+
+    fn fuga(&mut self) {
+        self.name = "fuga".to_string();
     }
 }
 
 fn main() {
-    let persons = vec![
+    let mut persons = vec![
         Person::new("aaa"),
         Person::new("bbb"),
         Person::new("ccc"),
     ];
-    for p in &persons {
-        p.eat();
+    // for p in &persons {
+    //     p.eat();
+    // }
+    persons[0].hoge();
+    let mut hoge = &mut persons[0];
+    hoge.name = "12345".to_string();
+    println!("{}", hoge.name);
+    println!("{}", persons[0].name);
+    hoge = &mut persons[0];
+    println!("{}", hoge.name);
+    persons[0].eat();
+
+    let handles: Vec<_> = persons.into_iter().map(|p| {
+        thread::spawn(move || {
+            p.eat();
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
+
+        println!("end");
     }
 }
